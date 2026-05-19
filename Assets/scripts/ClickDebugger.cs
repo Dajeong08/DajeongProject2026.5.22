a1788e6b4f85f172d30321f24155b3bfd63db3f9
Dajeong08 <nk418236@gmail.com>
2026-05-20 02:23:10 +0900

    5월 20일 수정
    해야할것=>
    밸런스 조정 (1학년)
    적 애니메이션 위치 변경
    카드 이미지 변경(간단하게 공격, 방어, 공/방) (1학년)
    + 힐카드 만들기, 필살기 카드 만들기 보스 전에 획득
    카드 드로우 효과음
    모든 버튼에 적용되는 특정 소리
    승리 효과음
    패배 효과음
    클리어 효과음
    한번씩 공격 씹히는거 오류 고치기 (1학년한테 확인받기)

---
+++ Assets/scripts/ClickDebugger.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using System.Collections.Generic;
+
+public class ClickDebugger : MonoBehaviour
+{
+    void Update()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            PointerEventData pointerData = new PointerEventData(EventSystem.current);
+            pointerData.position = Mouse.current.position.ReadValue();
+
+            List<RaycastResult> results = new List<RaycastResult>();
+            EventSystem.current.RaycastAll(pointerData, results);
+
+            if (results.Count == 0)
+            {
+                Debug.Log("클릭했는데 아무것도 없음!");
+                return;
+            }
+
+            // ✅ 맨 위 오브젝트만 출력
+            Debug.Log($"클릭된 오브젝트: {results[0].gameObject.name}");
+        }
+    }
+}
